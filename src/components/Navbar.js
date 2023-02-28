b69026835e73dddcfa5bca9577b397e60e6b968c
DesiRorie <113218888+DesiRorie@users.noreply.github.com>
2023-02-27 20:18:19 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <header className="header">
-      <img className="headerImage" src="/images/trollface.png" alt="face" />
+      
       <h2 className="headerTitle"> Meme Generator</h2>
     </header>
   );
